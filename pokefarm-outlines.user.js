eac28fca348f71902b05f3f965371f7af7949e76
kaedeira <60208639+kaedeira@users.noreply.github.com>
2025-07-07 21:38:34 -0700

    Update pokefarm-outlines.user.js

--- pokefarm-outlines.user.js
+++ pokefarm-outlines.user.js
@@ -7,8 +7,7 @@
 // @match        https://pokefarm.com/*
 // @grant        GM_xmlhttpRequest
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
-// @updateURL
-// @downloadURL
+// @downloadURL  https://github.com/kaedeira/pokefarm-outlines/raw/refs/heads/main/pokefarm-outlines.user.js
 // @run-at document-start
 // ==/UserScript==
 /* eslint-disable */
